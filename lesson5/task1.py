7abee9ef3fcb7747f8c27848a55af87aa00c64b1
Tskon <tsekhmeister.k@gmail.com>
2018-10-04 20:19:07 +0300

    lesson 5 task 1

---
+++ lesson5/task1.py
@@ -0,0 +1,18 @@
+# Создайте модуль, а в нем — функцию, создающую директории dir_1 — dir_9 в папке, из которой запущен данный скрипт.
+# Напишите вторую функцию, удаляющую эти папки. Проверьте работу функций в этом же модуле.
+
+import os
+
+def createDirs():
+  for i in range(1, 10):
+    dir = os.path.dirname(__file__) + '/dir_' + str(i)
+    os.mkdir(dir)
+
+def delDirs():
+  for i in range(1, 10):
+    dir = os.path.dirname(__file__) + '/dir_' + str(i)
+    os.rmdir(dir)
+
+createDirs()
+delDirs()
+
