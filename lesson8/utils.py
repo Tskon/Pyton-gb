1412a54baa3a4ade790e38704778002992c1a321
Tskon <tsekhmeister.k@gmail.com>
2018-10-18 19:34:56 +0300

    lesson 8 task 2

--- lesson8/utils.py
+++ lesson8/utils.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+import datetime
 
 
 def create_file(name, text=None):
@@ -21,8 +23,37 @@ def get_list(folders_only=False):
   print(result)
 
 
+def delete_file(name):
+  if os.path.isdir(name):
+    os.rmdir(name)
+  else:
+    os.remove(name)
+
+
+def copy_file(name, new_name):
+  if os.path.isdir(name):
+    try:
+      shutil.copytree(name, new_name)
+    except FileExistsError:
+      print('Такая папка уже есть')
+  else:
+    shutil.copy(name, new_name)
+
+
+def save_info(message):
+  current_time = datetime.datetime.now()
+  result = f'{current_time} - {message}'
+  with open('log.txt', 'a', encoding='utf-8') as f:
+    f.write(result + '\n')
+
+
 if __name__ == '__main__':
-  create_file('text.dat', 'text')
+  create_file('text.dat', 'text22')
   create_folder('fff')
+  copy_file('fff', 'fff1')
+  copy_file('text.dat', 'text1.dat')
+  delete_file('fff')
+  delete_file('text.dat')
   get_list()
   get_list(True)
+  save_info('some text')
